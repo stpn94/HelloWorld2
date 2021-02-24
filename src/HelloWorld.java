dc7d5a577a699cbea4089835de3a064efa90c3e2
jylee <stpn94@gmail.com>
2021-02-24 15:19:38 +0900

    세번째 라인 Hello end DanceHall추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("World's end Dancehall");
-		System.out.println("Earth's end Dancehall");
+		System.out.println("Hello end Dancehall");
 	}
 
 }
